2b7900feb076a2284bc006154030d4689c2f1575
Emily Bautista <emilybautista23@augustana.edu>
2026-03-03 12:40:51 -0600

    Connected Supabase onto the project

--- styled/lib/main.dart
+++ styled/lib/main.dart
@@ -1,6 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  await Supabase.initialize (
+    url: 'https://ksztxqxsylspvicdphzh.supabase.co',
+    anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtzenR4cXhzeWxzcHZpY2RwaHpoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI1NTcyOTYsImV4cCI6MjA4ODEzMzI5Nn0.IWntG-60ZINeOY1BoZRhw1iJhCxWlKuUw2JOoaDqezc',
+  );
+  
   runApp(const MainApp());
 }
 
