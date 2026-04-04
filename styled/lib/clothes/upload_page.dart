62b11b192ade1b176aca89a44a54ee200d09ecc4
Cesar <cesarjimenez23@augustana.edu>
2026-04-04 14:56:51 -0500

    Color Borders and Icons Changes

--- styled/lib/clothes/upload_page.dart
+++ styled/lib/clothes/upload_page.dart
@@ -40,18 +40,18 @@ class _UploadPageState extends State<UploadPage> {
 //Color Options
 //Based on the target.com color filter
 final List<Map<String, dynamic>> allColors = [
-  {'name':'Beige', 'color': const Color(0xFFF5F5DC)},
+  {'name':'Beige', 'color': const Color(0xFFE8D8B5)},
   {'name':'Black', 'color': Colors.black},
   {'name':'Blue', 'color': Colors.blue},
   {'name':'Brown', 'color': Colors.brown},
-  {'name':'Clear', 'color': Colors.white},
+  {'name':'Clear', 'color': const Color.fromARGB(255, 235, 240, 255)},
   {'name':'Gold', 'color': const Color.fromARGB(255, 191, 162, 0)},
   {'name':'Gray', 'color': const Color.fromARGB(255, 141, 141, 133)},
   {'name':'Green', 'color': Colors.green},
   {'name':'Multicolored', 'color': const Color(0xFFF4F4DC)},
-  {'name':'Off-white', 'color': const Color(0xFFF5F5DC)},
+  {'name':'Off-white', 'color': const Color(0xFFF2F2F2)},
   {'name':'Orange', 'color': Colors.orange},
-  {'name':'Pink', 'color': Colors.pink},
+  {'name':'Pink', 'color': const Color.fromARGB(255, 255, 110, 158)},
   {'name':'Purple', 'color': Colors.purple},
   {'name':'Red', 'color': Colors.red},
   {'name':'Silver', 'color': const Color.fromARGB(255, 75, 75, 62)},
@@ -325,10 +325,26 @@ final List<Map<String, dynamic>> allColors = [
                         }),
                     ),
                     title: Text(item['name']),
-                    trailing: CircleAvatar( //Circle avatar is a flutter widget that draws a filled circle
-                      backgroundColor: item['color'] as Color, //the color comes from the value defined in the allCOlors list
-                      radius: 14
+                    trailing: Container(
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2
+                        )
+                      ),
+                      child: CircleAvatar(
+                        radius: 14,
+                        backgroundColor: item['name'] == 'Multicolored'
+                        ? Colors.transparent
+                        : item['color'] as Color,
+                        backgroundImage: item['name'] == 'Multicolored'
+                        ? const AssetImage('assets/icons/multi.png')
+                        :null,
+                      
+                        ),
                     ),
+                      
 
                     onTap: () => setState(() { //select color when tapping anywhere on that specific row
                       if (isSelected) {
